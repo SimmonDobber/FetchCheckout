99ab0f3e03e7f31f3499e95581a77553e014859e
simmondobber <wojciechfbaranowski@gmail.com>
2023-08-15 18:09:31 +0200

    working version of the script

---
+++ fetchCheckout.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SUFFIX=''
+NAME=''
+
+while getopts 's:n:' flag; do
+    case "${flag}" in
+        s) SUFFIX="${OPTARG}" ;;
+        n) NAME="${OPTARG}" ;;
+    esac
+done
+
+if [[ ! -z "$NAME" ]]; then
+    if [[ $NAME =~ [0-9]+ ]]; then
+        BRANCH=$(git branch -a | grep -v 'remotes' | grep -E [A-Z]+-$NAME[^0-9]+.*$SUFFIX$)
+    else
+        BRANCH=$(git branch -a | grep -v 'remotes' | grep $NAME)
+    fi
+    if [[ -z $BRANCH ]] || [[ ${#BRANCH[@]} -eq 0 ]]; then
+        echo Given branch has not been found!
+    elif [[ ${#BRANCH[@]} -eq 1 ]]; then
+        echo git fetch
+        echo git checkout $BRANCH
+        #git fetch
+        #git checkout $BRANCH
+    else
+        echo Ambigous branch name!
+    fi
+else
+    echo Empty branch name!
+fi
