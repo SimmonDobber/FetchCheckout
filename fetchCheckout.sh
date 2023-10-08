7668297ede263660bb009d4431ff16abc04388c9
simmondobber <wojciechfbaranowski@gmail.com>
2023-10-08 23:32:04 +0200

    text justification

--- fetchCheckout.sh
+++ fetchCheckout.sh
@@ -1,52 +1,52 @@
 #!/bin/bash
 
 PREFIX="remotes/origin/"
-USE_SUFFIX=''
 SUFFIX='_master'
+USE_SUFFIX=''
 UPDATE=''
 COPY=''
 NAME=${@: -1}
 
 while getopts 's:uhc' flag; do
-    case "${flag}" in
-        s) USE_SUFFIX="true" ;;
-        u) UPDATE="true" ;;
-		c) COPY="true" ;;
-        h) 
-            echo "Syntax: ./fetchCheckout [OPTIONS] [BRANCH_NAME]"
-            echo "Options:"
-            echo "-h    prints help,"
-            echo "-u    updates branch,"
-            echo "-s    searches only for branches ending with '_master' suffix,"
-            echo "-c 	creates _master branch from give branch."
-            exit 0 ;;
-    esac
+  case "${flag}" in
+    s) USE_SUFFIX="true" ;;
+    u) UPDATE="true" ;;
+    c) COPY="true" ;;
+    h)
+      echo "Syntax: ./fetchCheckout [OPTIONS] [BRANCH_NAME]"
+      echo "Options:"
+      echo "-h    prints help,"
+      echo "-u    updates branch,"
+      echo "-s    searches only for branches ending with '_master' suffix,"
+      echo "-c 	creates _master branch from give branch."
+      exit 0 ;;
+  esac
 done
 
 if [[ -z $UPDATE ]]; then
 	if [[ $NAME =~ ^[0-9]+$ ]]; then
 		if [[ ! -z $USE_SUFFIX ]]; then
-        	BRANCH=$(git branch -a | grep -E [A-Z]+-$NAME[^0-9]+.*$SUFFIX$ | sort | head -1 | awk '{print $NF}')
+      BRANCH=$(git branch -a | grep -E [A-Z]+-$NAME[^0-9]+.*$SUFFIX$ | sort | head -1 | awk '{print $NF}')
 		else
 			BRANCH=$(git branch -a | grep -E [A-Z]+-$NAME[^0-9]+.* | sort | head -1 | awk '{print $NF}')
 		fi
     else
-        BRANCH=$(git branch -a | grep -E ^[[:space:]]*$NAME$ | sort | head -1 | awk '{print $NF}')
+      BRANCH=$(git branch -a | grep -E ^[[:space:]]*$NAME$ | sort | head -1 | awk '{print $NF}')
     fi
     if [[ ! -z $BRANCH ]] && [[ ${#BRANCH[@]} -gt 0 ]]; then
-        BRANCH=${BRANCH#$PREFIX}
+      BRANCH=${BRANCH#$PREFIX}
 		if [[ ! -z $COPY ]]; then
 			git checkout -b $BRANCH$SUFFIX $BRANCH
 		else 
-			git fetch
-        	git checkout $BRANCH
-        	if [[ ! -z $UPDATE ]]; then
-            	git merge $(git branch -a | grep 'origin'/$BRANCH$)
-			fi
+      git fetch
+      git checkout $BRANCH
+      if [[ ! -z $UPDATE ]]; then
+        git merge $(git branch -a | grep 'origin'/$BRANCH$)
+      fi
 		fi
     else
-        echo Given branch has not been found!
+      echo Given branch has not been found!
     fi
 else
-    echo Empty branch name!
+  echo Empty branch name!
 fi
